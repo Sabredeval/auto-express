ca6f39d6e5c0360c06af93ab3cc2ec1342a74a76
psyms24 <psyms24@nottingham.ac.uk>
2024-07-15 14:23:45 +0100

    Fixing email funcs

--- netlify-functions/submit-form.js
+++ netlify-functions/submit-form.js
@@ -1,26 +1,42 @@
+// Use at least Nodemailer v4.1.0
+const nodemailer = require('nodemailer');
 
-const nodemailer = require("nodemailer");
+// Generate SMTP service account from ethereal.email
+nodemailer.createTestAccount((err, account) => {
+    if (err) {
+        console.error('Failed to create a testing account. ' + err.message);
+        return process.exit(1);
+    }
 
-const transporter = nodemailer.createTransport({
-  host: "imap.ethereal.email",
-  port: 993,
-  secure: false,
-  auth: {
-    user: "roman.beier15@ethereal.email",
-    pass: "r3BsJK8FUDbpZbRcHf",
-  },
+    console.log('Credentials obtained, sending message...');
+
+    let transporter = nodemailer.createTransport({
+        host: smtp.ethereal.email,
+        port: 587,
+        secure: false,
+        auth: {
+            user: "malvina.cremin33@ethereal.email",
+            pass: "duFrfhpVWMAPX9uXwQ"
+
+        }
+    });
+
+    let message = {
+        from: 'Sender Name <malvina.cremin33@ethereal.email>',
+        to: 'Recipient <sabredeval@gmail.com>',
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    };
+
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Error occurred. ' + err.message);
+            return process.exit(1);
+        }
+
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
 });
-
-async function main() {
-  const info = await transporter.sendMail({
-    from: '"Maddison Foo Koch 👻" <roman.beier15@ethereal.email>', // sender address
-    to: "sabredeval@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-}
-
-main().catch(console.error);
